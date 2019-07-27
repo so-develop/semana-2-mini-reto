3b8783cc3651d41cae0090e93b283832196b511f
Saul Ruiz <saulruiz-123@hotmail.com>
2019-07-26 20:09:24 -0500

    Initial Commit

---
+++ semana 2: mini retoTests/semana_2__mini_retoTests.swift
@@ -0,0 +1,36 @@
+//
+//  semana_2__mini_retoTests.swift
+//  semana 2: mini retoTests
+//
+//  Created by Saul Ruiz on 7/26/19.
+//  Copyright © 2019 Saul Ruiz. All rights reserved.
+//
+
+import XCTest
+@testable import semana_2__mini_reto
+
+class semana_2__mini_retoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
